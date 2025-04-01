515eca95dc7ad245f4c10366e9060f56eda51229
EnLin06 <newbiesky1121@gmail.com>
2025-04-01 15:02:18 +0800

    Update data_set.py

--- data_set.py
+++ data_set.py
@@ -4,18 +4,13 @@ def data_set(database):
     pass_count = {}
     for id, schools in database.items():
         for school in schools:
-            if id == 'pass_count':
-                pass
-            else:
-                if school not in pass_count:
-                    pass_count[f'{school}'] = [0]
-                pass_count[f'{school}'].append(id)
-                pass_count[f'{school}'][0] += 1
-
-    database['pass_count'] = pass_count
+            if school not in pass_count:
+                pass_count[f'{school}'] = [0]
+            pass_count[f'{school}'].append(id)
+            pass_count[f'{school}'][0] += 1
 
     with open('For_Fun/json/passcount.json', 'w', encoding='utf-8') as f:
-        json.dump(database['pass_count'], f, ensure_ascii=False, indent=2)
+        json.dump(pass_count, f, ensure_ascii=False, indent=2)
 
     del database['pass_count']
     with open('For_Fun/json/database.json', 'w', encoding='utf-8') as f:
